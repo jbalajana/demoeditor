13478afca9f9b039ab4cd97a265e55eef1a96ed2
balaji <jbala.jana@gmail.com>
2020-06-13 18:55:19 +0530

    Updates

---
+++ precache-manifest.6a0c194e0eb0ab7e608dd08e4a434793.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94b2a77e4c6510d8914a1ac93522f58",
+    "url": "/demoeditor/index.html"
+  },
+  {
+    "revision": "e8e842804032585e5e27",
+    "url": "/demoeditor/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe6ff4d94549a97c4593",
+    "url": "/demoeditor/static/js/2.f192ddac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demoeditor/static/js/2.f192ddac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e842804032585e5e27",
+    "url": "/demoeditor/static/js/main.e446e3cf.chunk.js"
+  },
+  {
+    "revision": "c77980393a40b2eb7028",
+    "url": "/demoeditor/static/js/runtime-main.3b633d58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demoeditor/static/media/logo.5d5d9eef.svg"
+  }
+]);
